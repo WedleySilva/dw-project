e72b98cddf680d30c7ff7fc46b1cd67dae7fabd8
WedleySilva <wedleysilva2105@gmail.com>
2022-06-24 11:30:52 -0300

    master

--- src/views/TelaLogin.vue
+++ src/views/TelaLogin.vue
@@ -2,12 +2,24 @@
 export default {};
 </script>
 <template>
-  <div class="box-login">
+  <div class="center">
     <h1>Login</h1>
-    <input autocomplete="off" name="Email" id="Email" class="input" type="email"/>
-    <input type="password" class="password" />
-    <button class="blogin" type="submit"></button>
+    <form method="post">
+      <div class="txt_field">
+        <input type="email" required />
+        <span></span>
+        <label>Email</label>
+      </div>
+      <div class="txt_field">
+        <input type="password" required />
+        <span></span>
+        <label>Senha</label>
+      </div>
+      <div class="pass">Esqueçeu a Senha?</div>
+      <input type="submit" value="login" />
+      <div class="signup_link">
+        Não possui Conta? <a href="#">Tenha uma Conta</a>
+      </div>
+    </form>
   </div>
 </template>
-<style>
-</style>
